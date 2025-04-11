f091e2c23281153bf8d57d1322d8863969ebd706
harsh1745-hub <harshsrivastavza1711@gmail.com>
2025-04-11 16:02:24 +0530

    Update api.js

--- frontend/src/API/api.js
+++ frontend/src/API/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/study", 
+    baseURL: "https://edtech-35vd.onrender.com", 
     headers: {
       "Content-Type": "application/json",
     },
