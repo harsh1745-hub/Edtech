75a935b7b9cfd577084a28693d7701d8c908a1c6
harsh1745-hub <harshsrivastavza1711@gmail.com>
2025-04-26 12:45:12 +0530

    Update videoapi.js

--- frontend/src/API/videoapi.js
+++ frontend/src/API/videoapi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/", 
+    baseURL: "https://edtech-f05f.onrender.com/", 
     headers: {
       "Content-Type": "application/json",
     },
